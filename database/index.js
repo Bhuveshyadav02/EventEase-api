51c9ecbab260f6323f909ad39428a9456af406b9
Sarvesh Yadav <sarveshyadav1v@gmail.com>
2024-08-22 22:19:20 +0530

    Database Url connection fiexd

--- database/index.js
+++ database/index.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
-const url =
-  "mongodb+srv://bhuvesh:1234@nodeexpressprojects.mdmrvlx.mongodb.net/HallBooking?retryWrites=true&w=majority&appName=NodeExpressProjects";
+
+  
 const connectDB = async () => {
-  // console.log(url)
+  const url =process.env.MONGODB
+   console.log(url)
   return mongoose.connect(url, {});
 };
 
