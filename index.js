2d83ab6a3c0f625c7ba1fd628cbf8ef3b41c297a
Sarvesh Yadav <sarveshyadav1v@gmail.com>
2024-08-23 21:46:29 +0530

    Fix CORS policy

--- index.js
+++ index.js
@@ -1,58 +1,44 @@
-require('dotenv').config()
+require('dotenv').config();
 const express = require('express');
 const app = express();
-const connectDB = require("./database/index");
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet');
+app.use(express.static(path.join(__dirname, 'public')));
+// CORS Configuration
+const corsOptions = {
+  origin: "*", // Allow any origin
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true,
+};
 
+// Middleware
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
-const corsOptions = {
-  origin: ['http://localhost:3000','https://eventease-gw9qwfwee-bhuveshs-projects.vercel.app'],
-  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', 
-  credentials: true, 
-};
-app.use(cors(corsOptions))
-app.options('*', cors(corsOptions));
-
-app.set("trust proxy", 1);
-
-// Helmet configuration for CSP
 app.use(helmet());
-app.use(helmet.contentSecurityPolicy({
-  directives: {
-    defaultSrc: ["'self'"],
-    scriptSrc: ["'self'"],
-    styleSrc: ["'self'", "https://fonts.googleapis.com"],
-    fontSrc: ["'self'", "https://fonts.gstatic.com"],
-    imgSrc: ["'self'", "data:"],
-    connectSrc: ["'self'", "*",  "http://localhost:3000" ], // Allow connections from any origin
-    objectSrc: ["'none'"],
-    upgradeInsecureRequests: [],
-  }
-}));
-
 
 // Import and use routes
 const authRouter = require("./routes/authRoutes");
-const hallRouter=require("./routes/hallRoutes")
-const bookingRouter=require('./routes/bookingRoutes')
+const hallRouter = require("./routes/hallRoutes");
+const bookingRouter = require('./routes/bookingRoutes');
 app.use(authRouter);
-app.use(hallRouter)
-app.use(bookingRouter)
+app.use(hallRouter);
+app.use(bookingRouter);
 
+// Health Check Route
 app.get("/", (req, res) => {
   res.send("Server is working");
 });
 
+// Start Server
 const start = async () => {
   try {
-    await connectDB(); // Ensure to await the database connection
+    // Ensure to await the database connection
     console.log("Database Connected");
     const PORT = process.env.PORT || 5000;
     app.listen(PORT, () => {
-      console.log("Server Started at Port 5000");
+      console.log(`Server Started at Port ${PORT}`);
     });
   } catch (error) {
     console.log(error);
